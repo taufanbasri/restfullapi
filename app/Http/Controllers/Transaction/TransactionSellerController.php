418a5ad2cfda033add168b81b86ad1506a574cb9
vagrant <vagrant@homestead>
2017-08-22 03:08:29 +0000

    Implementing the Operations & complex Operations for Transaction

---
+++ app/Http/Controllers/Transaction/TransactionSellerController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Transaction;
+
+use App\Transaction;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class TransactionSellerController extends ApiController
+{
+
+    public function index(Transaction $transaction)
+    {
+        $seller = $transaction->product->seller;
+
+        return $this->showOne($seller);
+    }
+}
